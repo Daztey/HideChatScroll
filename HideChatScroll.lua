c833dd5f273d622c96666fce807c8466ae9eb59f
Daztey <41431889+Daztey@users.noreply.github.com>
2018-07-19 14:07:55 +0200

    Add files via upload

---
+++ HideChatScroll.lua
@@ -0,0 +1,4 @@
+local b = ChatFrame1.ScrollToBottomButton b:UnregisterAllEvents() b:SetScript("OnShow", b.Hide) b:Hide()
+local b = ChatFrame1.ScrollBar b:UnregisterAllEvents() b:SetScript("OnShow", b.Hide) b:Hide()
+local b = ChatFrame3.ScrollToBottomButton b:UnregisterAllEvents() b:SetScript("OnShow", b.Hide) b:Hide()
+local b = ChatFrame3.ScrollBar b:UnregisterAllEvents() b:SetScript("OnShow", b.Hide) b:Hide()
